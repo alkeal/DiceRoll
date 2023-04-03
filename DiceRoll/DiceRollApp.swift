a2d6b7120f260eddcdec945e5d7988ee8e0b0123
a <yhma22.alkeal@folkuniversitetet.nu>
2023-04-03 13:57:09 +0200

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by A on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
